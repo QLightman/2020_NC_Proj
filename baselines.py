473fdcf6b6c06976dbe2b20e71960f49c2b323ec
Tim <xyshen@umich.edu>
2020-05-02 23:52:36 -0500

    minor

--- baselines.py
+++ baselines.py
@@ -3,9 +3,11 @@ from keras_segmentation.models.unet import vgg_unet
 model = vgg_unet(n_classes=2, input_height=512, input_width=512)
 
 model.train(
-    train_images =  'data/membrane/train/image',
-    train_annotations = 'data/membrane/train/aug',
-    epochs=5
+    train_images = 'data/membrane/train/image',
+    train_annotations = 'data/membrane/train/label',
+    checkpoints_path = 'checkpoints/',
+    epochs = 1,
+    steps_per_epoch = 100
 )
 
 out = model.predict_multiple(
--- tiff.py
+++ tiff.py
@@ -3,5 +3,7 @@ import skimage.io as io
 from skimage.external import tifffile
 
 with tifffile.TiffWriter('pred.tif') as stack:
-    for filename in glob.glob('data/membrane/423_test/*.png'):
-        stack.save(io.imread(filename))
+    for fid in range(30):
+        filename = 'data/membrane/423_test/{:d}_predict.png'.format(fid)
+        # filename = 'data/membrane/423_test/{:d}.png'.format(fid)
+        stack.save(io.imread(filename, as_gray=True))
