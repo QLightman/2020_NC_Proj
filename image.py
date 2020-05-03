d283628c8502ba57ce6d86772ae49865235c73ca
Tim <xyshen@umich.edu>
2020-05-03 01:36:48 -0500

    minor

--- image.py
+++ image.py
@@ -8,20 +8,19 @@ def tiff(form='data/membrane/pred/{:d}.png'):
     with tifffile.TiffWriter('pred.tif') as stack:
         for fid in range(30):
             filename = form.format(fid)
-            img = io.imread(filename)[:, :, 2] * 255
+            img = io.imread(filename) * 255
             stack.save(img)
 
 
 def convert():
     src = 'data/membrane/train/label/'
     dst = 'data/membrane/train/label_rgb/'
-    img = np.zeros((512, 512, 3), dtype=np.uint8)
     for fid in range(30):
         filename = src + str(fid) + '.png'
-        img[:, :, 2] = (io.imread(filename) / 255)
+        img = (io.imread(filename) / 255).astype(np.uint8)
         io.imsave(dst + str(fid) + '.png', img)
 
 
 if __name__ == '__main__':
-    # tiff()
-    convert()
+    tiff()
+    # convert()
